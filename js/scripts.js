9374360f0b10de4cc16463bd22e9e51beb06d248
Caitlin Hines <caitlinhines@me.com>
2017-06-20 15:41:59 -0700

    accordian drop down working for side panel

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 
+$(document).ready(function(){
+    $("#accordianmenu p").click(function(){
+		$("#accordianmenu ul ul").slideUp();
+		if(!$(this).next().is(":visible"))
+		{
+			$(this).next().slideDown();
+		}
+	});
+});
